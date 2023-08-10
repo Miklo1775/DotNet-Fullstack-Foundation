54d702f9c5a267900cd890268d718edf200ae6da
Miklo1775 <thelegendofvictor@gmail.com>
2023-08-10 12:40:32 -0500

    Uploading progress for Week 1 Module ASP.NET Fundamentals

---
+++ Backend/Week_1/coreWebApp/coreWebAppMVC/Controllers/CustomerController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using coreWebAppMVC.Models;
+
+namespace coreWebAppMVC.Controllers
+{
+    public class CustomerController : Controller
+    {
+        public static List<Customer> customer = new List<Customer>()
+        {
+            new Customer() {Id = 101, Name = "Miklo", Amount = 50}
+        };
+        public IActionResult Index()
+        {
+            ViewBag.Message = "Customer Management System";
+            ViewBag.CustomerCount = customer.Count();
+            ViewBag.CustomerList = customer;
+            return View();
+        }
+
+        [Route("~/")]
+        public IActionResult Details()
+        {
+            return View();
+        }
+
+
+        public IActionResult Message()
+        {
+            return View();
+        }
+
+
+    }
+}
