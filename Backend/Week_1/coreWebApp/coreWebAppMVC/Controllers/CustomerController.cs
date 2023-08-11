280b1913957dd0d370ffa153f104026d0bea568c
Miklo1775 <thelegendofvictor@gmail.com>
2023-08-11 14:06:08 -0500

    Uploading Progress for Week 1ASP.NET Core fundamentals

--- Backend/Week_1/coreWebApp/coreWebAppMVC/Controllers/CustomerController.cs
+++ Backend/Week_1/coreWebApp/coreWebAppMVC/Controllers/CustomerController.cs
@@ -7,7 +7,8 @@ namespace coreWebAppMVC.Controllers
     {
         public static List<Customer> customer = new List<Customer>()
         {
-            new Customer() {Id = 101, Name = "Miklo", Amount = 50}
+            new Customer() {Id = 101, Name = "Miklo", Amount = 50},
+            new Customer() {Id = 102, Name = "Chichi", Amount = 100}
         };
         public IActionResult Index()
         {
@@ -17,18 +18,65 @@ namespace coreWebAppMVC.Controllers
             return View();
         }
 
-        [Route("~/")]
         public IActionResult Details()
         {
+            ViewData["Message"] = "Details Pagerino";
+            ViewData["CustomerCount"] = customer.Count();
+            ViewData["CustomerList"] = customer;
             return View();
         }
 
+        [Route("~/")]
+        public IActionResult Temp()
+        {
+            TempData["Message"] = "Details Pagerino";
+            TempData["CustomerCount"] = customer.Count();
+            TempData["CustomerList"] = customer;
+            return View();
+        }
+
+        public IActionResult Method2()
+        {
+            if (TempData["Message"] == null) {
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.Message = TempData["Message"].ToString();
+            return View();
+        }
 
         public IActionResult Message()
         {
             return View();
         }
 
+        public IActionResult Login()
+        {
+            HttpContext.Session.SetString("username", "Miklo");
+            return RedirectToAction("Success");
 
+        }
+
+        public IActionResult Success()
+        {
+            ViewBag.Username = HttpContext.Session.GetString("username");
+            return View();
+        }
+
+        public IActionResult Logout()
+        {
+            HttpContext.Session.Remove("username");
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult QueryTest()
+        {
+            string name = "Miklo";
+            if (!String.IsNullOrEmpty(HttpContext.Request.Query["name"])) { 
+                name = HttpContext.Request.Query["name"];
+            }
+            ViewBag.Name = name;
+            return View();
+        }
     }
 }
