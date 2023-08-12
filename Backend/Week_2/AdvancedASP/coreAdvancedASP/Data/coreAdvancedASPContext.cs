1c86bc75ce57ab69fe86122dea61e85ac98dca35
Miklo1775 <thelegendofvictor@gmail.com>
2023-08-12 11:06:35 -0500

    Uploading progress for Week 2 Advanced ASP

---
+++ Backend/Week_2/AdvancedASP/coreAdvancedASP/Data/coreAdvancedASPContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using coreAdvancedASP.Models;
+
+namespace coreAdvancedASP.Data
+{
+    public class coreAdvancedASPContext : DbContext
+    {
+        public coreAdvancedASPContext (DbContextOptions<coreAdvancedASPContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<coreAdvancedASP.Models.User> User { get; set; } = default!;
+    }
+}
