c852cfc0522d1ee074378314a34da24934d1dfdb
Miklo1775 <thelegendofvictor@gmail.com>
2023-08-15 15:23:54 -0500

    Uploading last of progress for Week 2 module MVC Pipeline

---
+++ Backend/Week_2/ASPMVCPipelineSecurity/coreIDApplication/Controllers/DemoController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace coreIDApplication.Controllers
+{
+    //[AllowAnonymous]
+    public class DemoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public IActionResult Details() {
+            
+            return View();
+        }
+
+        [Authorize]
+        public IActionResult Policy()
+        {
+            return View();
+        }
+    }
+}
